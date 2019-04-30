cd189265f8b88bd575ac5fc14de1398c6f1a9b73
snupa <adrian@unloq.io>
2019-04-30 13:28:10 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict';
+/**
+ * The msgbin project tries to perform message encoding-decoding using ArrayBuffers/Buffers(node).
+ * This is currently in experimental mode
+ * */
+module.exports = {};
